8dbf1e9c81d25806bc64ee627827cb728a0649f2
kaddu341 <awwabaliazam@gmail.com>
2025-03-21 16:54:08 -0400

    added user login page

--- src/users/views.py
+++ src/users/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import redirect, render
 
 
@@ -7,8 +8,19 @@ def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("home")
     else:
         form = UserCreationForm()
     return render(request, "users/register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+    return render(request, "users/login.html", {"form": form})
